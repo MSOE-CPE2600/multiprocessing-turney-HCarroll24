f5c59ec33e0fe26d4b831437f95694f34721f1c8
Hunter Carroll <carrollh@msoe.edu>
2025-11-23 15:46:30 -0600

    adding typedef struct for thread data

--- mandel_movie.h
+++ mandel_movie.h
@@ -7,3 +7,19 @@
  * Section: 012
  * Assignment Lab 12
 **********************************************************/
+#ifndef MANDEL_MOVIE_h
+#define MANDEL_MOVIE_h
+
+#include "jpegrw.h"
+
+// struct for thread data
+typedef struct {
+    imagRawImage* img;
+    double xmin;
+    double xmax;
+    double ymin;
+    double ymax;
+    int max;
+    int start_row;
+    int end_row;
+} thread_data_t;
